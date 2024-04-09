960e9461eb3579571941cb89382c63c54fbf4553
philip <ajibolaphilip10@gmail.com>
2024-04-09 22:36:26 +0100

    commit

--- src/main/java/org/africaSemicolon/data/repository/Expenses.java
+++ src/main/java/org/africaSemicolon/data/repository/Expenses.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface Expenses extends MongoRepository<Expense,String> {
     Expense findByExpenseTitleAndExpenseOwnerName(String expenseTitle, String username);
 
+    boolean existsByTitleAndExpenseOwnerName(String expenseTitle, String expenseOwnerName);
 }
