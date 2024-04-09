b89ac9cb35c5c3583380e58f43ebd20189d58a02
philip <ajibolaphilip10@gmail.com>
2024-04-09 22:35:44 +0100

    commit

---
+++ src/main/java/org/africaSemicolon/exception/ExpenseNotFoundException.java
@@ -0,0 +1,7 @@
+package org.africaSemicolon.exception;
+
+public class ExpenseNotFoundException extends ExpenseTrackerException {
+    public ExpenseNotFoundException(String message) {
+        super(message);
+    }
+}
